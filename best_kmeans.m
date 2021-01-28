e4165e8c891268c66c7a10f6de1b028643d2ebf1
AhlamMallak <78078783+AhlamMallak@users.noreply.github.com>
2021-01-28 20:49:27 +0100

    Update best_kmeans.m

--- best_kmeans.m
+++ best_kmeans.m
@@ -1,16 +1,4 @@
 function [IDX,C,SUMD,D,K]=best_kmeans(X)
-% [IDX,C,SUMD,K] = best_kmeans(X) partitions the points in the N-by-P data matrix X
-% into K clusters. Rows of X correspond to points, columns correspond to variables. 
-% IDX containing the cluster indices of each point.
-% C is the K cluster centroids locations in the K-by-P matrix C.
-% SUMD are sums of point-to-centroid distances in the 1-by-K vector.
-% K is the number of cluster centriods determined using ELBOW method.
-% ELBOW method: computing the destortions under different cluster number counting from
-% 1 to n, and K is the cluster number corresponding 90% percentage of
-% variance expained, which is the ratio of the between-group variance to
-% the total variance. see <http://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set>
-% After find the best K clusters, IDX,C,SUMD are determined using kmeans
-% function in matlab.
 
 dim=size(X);
 disp('dim');
